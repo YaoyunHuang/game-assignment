e9a4942d637caf21899829d64a2f4c312578071c
ly0121 <aka6208@yyyMacBook-Air.local>
2026-05-24 20:16:18 +1000

    Complete question 2

--- q1.R
+++ q1.R
@@ -46,3 +46,42 @@ getwd()
 list.files()
 list.files("/Users/aka6208/Desktop", pattern = "\\.R$")
          
+
+
+library(dplyr)
+library(ggplot2)
+library(knitr)
+# Read the game.csv file
+# and save it into a dataframe called games
+  games <- read.csv("games.csv") 
+  
+  # Take the sessions dataframe 
+  # then join the games dataframe 
+  # using the common column game_id 
+  genre_data <- sessions %>%
+    left_join (games, by = "game_id")
+  
+  #take the joined data
+  #then group bu the data by genre 
+  genre_summary <- genre_data %>%
+  group_by (genre) %>%
+    
+    #create summary statistics for each genre 
+    summarise(
+      
+      average_play_time = mean(play_time_minutes, na.rm = TRUE),
+      average_score = mean(score, na.rm= TRUE),
+      number_session = n(), 
+      unique_players = n_distinct(player_id)
+    )
+  ggplot(
+    genre_summary,
+    aes(
+      x=genre,
+      y=unique_players,
+      fill = genre
+    )
+  )+
+    geom_bar(stat = "identity"
+    )
+  )
